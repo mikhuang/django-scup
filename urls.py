3c15f1416a40bbd069ed9604a78cc9f4d876372d
Michael Huang <mikhuang@gmail.com>
2011-10-11 19:46:06 -0700

    first

---
+++ urls.py
@@ -0,0 +1,22 @@
+from django.conf.urls.defaults import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'scup.views.home', name='home'),
+    # url(r'^scup/', include('scup.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    
+    (r'^materials', 'scup.materials.views.index'),
+    
+    # for django-fiber
+    (r'^api/v1/', include('fiber.api.urls')),
+    (r'^admin/fiber/', include('fiber.admin_urls')),
+    (r'^jsi18n/$', 'django.views.i18n.javascript_catalog', {'packages': ('fiber',),}),
+)
